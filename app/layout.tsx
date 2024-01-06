0ed99f63b34608439b6df5dcb65f88b68ba73754
YYGod <2569630663@qq.com>
2024-01-06 21:49:23 +0800

    :lipstick: (字体) 新字体✌

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,24 @@
 import "./globals.css";
+import localFont from "next/font/local";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
+const roboto = localFont({
+  src: [
+    {
+      path: "./fonts/LXGWWenKailite-Regular.ttf",
+      style: "regular",
+    },
+    {
+      path: "./fonts/LXGWWenKaiLite-Light.ttf",
+      style: "light",
+    },
+    {
+      path: "./fonts/LXGWWenKaiLite-Bold.ttf",
+      style: "bold",
+    },
+  ],
+  display: "swap",
+});
 export default function RootLayout({
   children,
 }: {
@@ -9,7 +26,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <Header></Header>
         {children}
         <Footer></Footer>
--- components/Header.tsx
+++ components/Header.tsx
@@ -29,15 +29,15 @@ export default function Header() {
   ];
   return (
     <div className="bg-white w-full flex items-center justify-center h-16">
-      <div className=" flex w-[65vw] justify-between text-2xl ">
-        <Link href="/" className="text-visit-font">
+      <div className=" flex w-[60vw] justify-between text-2xl ">
+        <Link href="/" className="text-visit-font font-bold">
           YYGod0120
         </Link>
         <div className="flex justify-between text-xl w-[28vw] text-default-font">
           {tags.map((item) => {
             return (
               <Link
-                className="hover:text-visit-font"
+                className="hover:text-visit-font font-semibold"
                 href={item.href}
                 key={item.name}
               >
