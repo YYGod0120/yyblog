2dbe18ad4047955d5365489a14d063bab7135ae7
YYGod <2569630663@qq.com>
2024-01-27 13:32:25 +0800

    :sparkles: (new 404 page)

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+"use client";
+import "../styles/error.css";
+export default function GlobalError() {
+  return (
+    <div className="flex flex-row justify-center items-center w-[100vw] h-[82vh]">
+      <div title="404" id="error">
+        404
+      </div>
+    </div>
+  );
+}
