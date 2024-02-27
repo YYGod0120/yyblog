76b7af0d8f35eb09f67c9e4318f37540f9c41502
YYGod <2569630663@qq.com>
2024-02-27 11:04:29 +0800

    :bug: (样式问题)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,16 @@
 import { YEARRANGE } from "@/utils/time";
 import React from "react";
 import Image from "next/image";
-import "../styles/hover-underline-animation.css";
+import "@/styles/hover-underline-animation.css";
 export default function Footer() {
   return (
-    <div className="mt-5 flex">
+    <div className=" mt-5 flex">
       © {YEARRANGE}
       <div className=" ml-6 flex flex-row ">
         <Image src="/github.png" alt="github" width={24} height={24}></Image>
         <a
           href="https://github.com/YYGod0120"
-          className=" hover-underline-animation text-default-font no-underline hover:text-default-font"
+          className="hover-underline-animation text-default-font no-underline"
         >
           YYGod0120
         </a>
