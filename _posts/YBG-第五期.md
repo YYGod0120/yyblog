481a6e1ae95efe38e720772f59432fcbadbc7dce
YYGod <2569630663@qq.com>
2024-03-04 09:38:36 +0800

    :pencil: (YBG-第五期)

---
+++ _posts/YBG-第五期.md
@@ -0,0 +1,53 @@
+---
+title: YBG-第五期
+date: 2024-03-04
+categories: Weekly
+excerpt: 这里是每周总结，记录每一周的趣事，技术分享以及学习生活。
+---
+
+> 傲不可长，志不可满
+
+## Technology
+
+1.  为每个开发人员以及IT工作者来说非常有用的工具集合网站，包括不仅限于TOKEN生成器,hash文本,加密等等工具。
+
+    https://it-tools.tech/
+
+2.  一些能提升效率的vscode插件。
+
+    https://www.freecodecamp.org/news/best-vscode-extensions/?ref=dailydev
+
+3.  React从类组件到Hook，马上又要到compile时代
+
+    https://reacttraining.com/blog/react-19-will-be-compiled?ref=dailydev
+
+4.  React 2024年的趋势
+
+    https://mp.weixin.qq.com/s/s0-7yD_gspgNX7RYvn_wxA
+
+5.  Vue十年回顾，来自Even You的演讲
+
+    https://mp.weixin.qq.com/s/s0-7yD_gspgNX7RYvn_wxA
+
+6.  Node.js，2023年回顾。
+
+    https://blog.rafaelgss.dev/nodejs-2023-year-in-review
+
+## Study
+
+1. [《Concise TypeScript Book》](https://typescript-book.vercel.app/zh-cn/?ref=dailydev): 一本完全开源免费，全面简洁的涵盖了TypeScript功能的电子书，无论你是新手还是经验丰富的开发人员，本书都是增强您对 TypeScript 的理解和熟练程度的宝贵资源。
+
+2. [前端进阶之旅](https://interview.poetries.top/): 不免费，但是内容多，范围广的面试宝典。要说缺点可能就是内容有些比较老，网页优化不行。但总体来说对于准备面试是必不可少的。
+
+## Life
+
+1. 本周又接到了一个VUE项目，没使用过VUE，但从这一周的体验来说，至少说还能依葫芦画瓢，不愧是适用于没学过前端的快速上手框架
+
+2. 闲下来的功夫给博客改了点bug以及新增了英语语言，只是翻译还没完全。
+
+   [React-i8next](https://react.i18next.com/) 官方网站居然不支持中文😥
+
+   [Next13及以上配置教程](https://locize.com/blog/next-app-dir-i18n/)
+
+3. NodeJS新官网：https://beta-node-js-org.vercel.app/en
+   <img src="/imgs/YBG-第五期/nodejs.png" alt="nodejs新官网"/>
---
+++ app/[language]/essay/2024-02-05/10/page.tsx
@@ -0,0 +1,90 @@
+
+  import Image from "next/image";
+import dynamic from "next/dynamic";
+  //@ts-ignore
+import SparkleMotion from "@/public/imgs/[YGB]-第一期/SparkleMotion.gif"
+//@ts-ignore
+import GhostCursor from "@/public/imgs/[YGB]-第一期/GhostCursor.gif"
+//@ts-ignore
+import gametime from "@/public/imgs/[YGB]-第一期/gametime.png"
+  // @ts-ignore
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// @ts-ignore
+import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import "@/app/[language]/essay/essay.css";
+export default function Page() {
+  const Comment = dynamic(() => import("@/app/[language]/components/Comment"), {
+    ssr: false,
+  });
+  return (
+    <div>
+    <div className="mt-8 bg-white flex flex-col items-start text-lg shadow-lg rounded-sm">
+    <span className="text-4xl text-left lg:px-20 md:px-[2.5vw] px-4 pt-12 text-visit-font font-bold">
+      YGB-第一期
+    </span>
+    <span className="text-[#86909C] lg:px-20 pt-5 px-4 text-xl mb-5 md:px-[2.5vw]">
+      Categories: Weekly &nbsp; &nbsp; 2024-02-05
+    </span>
+    <div className="flex text-start break-words flex-col pb-12 lg:px-20 lg:w-[740px] md:w-[90vw] md:px-[2.5vw] w-[95vw] px-[2.5vw]">
+      <blockquote>
+<p>这里是每周总结，记录每一周的趣事，技术分享以及学习生活。</p>
+</blockquote>
+<p>这个周刊的主要目的就是记录生活，分享技术以及当作一个小小的备忘录。
+也希望每周能顺利更新，先浅浅定个 15 期的目标。</p>
+<h2>Technology</h2>
+<ol>
+<li><p>一个有趣的烟花跟随样式</p>
+<p><a href="https://codepen.io/leimapapa/pen/XWGMrvo">https://codepen.io/leimapapa/pen/XWGMrvo</a></p>
+<Image src={SparkleMotion} alt="Sparkle Motion" 
+      sizes="100vw"
+      style={{
+        width: '100%',
+        height: 'auto',
+      }} />
+</li>
+<li><p>一只有趣的幽灵(也是我博客上的那只)</p>
+<p><a href="https://codepen.io/supah/pen/OBbzvp">https://codepen.io/supah/pen/OBbzvp</a></p>
+<Image src={GhostCursor} alt="Ghost Cursor" 
+      sizes="100vw"
+      style={{
+        width: '100%',
+        height: 'auto',
+      }} />
+</li>
+<li><p>Shiki：一个类似于 Highlight.js 的语法高亮器。</p>
+<p><a href="https://shiki-zh-docs.vercel.app/">https://shiki-zh-docs.vercel.app/</a>
+博客用<code>react-syntax-highlighter</code>语法高亮组件,使用 prismjs 或者 highlightjs 进行封装。新旧程度肯定不如 Shiki 新。</p>
+</li>
+<li><p>一个页面灵感的网站，会点设计对自己开发有不少帮助。
+<a href="https://uxchi.notion.site/UI-Inspiration-881b4c0179a74935a3f607ad3521cdb5">https://uxchi.notion.site/UI-Inspiration-881b4c0179a74935a3f607ad3521cdb5</a></p>
+</li>
+</ol>
+<h2>Study</h2>
+<ol>
+<li><p><a href="https://ishadeed.com/article/target-size#for-designers-delivering-a-clear-target-size-spec">设计更好的目标尺寸</a>：作为用户，我们总是要和按钮，链接以及卡片等等 UI 进行交互，一旦这些东西的大小不对，就会造成很大的困扰。作为前端开发，我们有必要了解一下一些设计方面的知识。</p>
+</li>
+<li><p><a href="https://arendjr.nl/2023/04/mvp-the-most-valuable-programmer">The Most Valuable Programmer</a>： 如何做一个 MVP 程序员，绝不是写最多的代码，也不是写最有价值的代码，而是成为一个最好的自己。</p>
+</li>
+</ol>
+<h2>Life</h2>
+<ol>
+<li><p><a href="https://yyblog-gamma.vercel.app/">NewBlog</a>：新博客搭建完备，功能不多，后面再说。预计年前替换之前那个旧博客。对这个博客感兴趣的可以去看看 github 上的代码仓库。</p>
+</li>
+<li><p>幻兽帕鲁火爆全中国，身边就没有没在玩的。98 大洋购入后 2 天疯玩 20 小时后又感觉索然无味，等后续更新了。</p>
+<Image src={gametime} alt="=game-time" 
+      sizes="100vw"
+      style={{
+        width: '100%',
+        height: 'auto',
+      }} /></li>
+</ol>
+
+    </div>
+    
+  </div>
+  <div className="mt-12 py-8 bg-white px-6">
+  <Comment />
+</div>
+    </div>
+  );
+}
