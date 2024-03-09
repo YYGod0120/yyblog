323c78275fde7f53de20bd989cf1fbbd42b6b9aa
YYGod <2569630663@qq.com>
2024-03-09 10:15:38 +0800

    :bug: (修复了seo问题)

--- app/[language]/layout.tsx
+++ app/[language]/layout.tsx
@@ -1,4 +1,6 @@
 import localFont from "next/font/local";
+import { Metadata } from "next";
+
 import Header, { Tag } from "@/app/[language]/components/Header";
 import Footer from "@/app/[language]/components/Footer";
 import { languages } from "@/app/i18n/setting";
@@ -13,6 +15,12 @@ const roboto = localFont({
 export async function generateStaticParams() {
   return languages.map((language) => ({ language }));
 }
+
+// either Static metadata
+export const metadata: Metadata = {
+  title: "YYGod0120’s Blog",
+  description: "这里是记录我的学习，技术以及生活的地方，欢迎大家一起交流讨论",
+};
 
 export default async function RootLayout({
   children,
