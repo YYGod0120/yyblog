ae563c00e5c657de997ace4356783df7587394fa
YYGod <2569630663@qq.com>
2024-05-20 15:57:32 +0800

    :bug: (Banner字体翻译问题)

--- app/[language]/components/TypeWrite.tsx
+++ app/[language]/components/TypeWrite.tsx
@@ -1,10 +1,10 @@
 import { usePathname } from "next/navigation";
 import { ReactTyped } from "react-typed";
 import { splitPathname } from "@/utils/dealPathname";
-import { useTranslation } from "@/app/i18n/client";
-export function MyTypeWrite({ language }: { language: string }) {
+import { useTranslation } from "@/app/i18n/";
+export async function MyTypeWrite({ language }: { language: string }) {
   const pathname = usePathname();
-  const { t } = useTranslation(language);
+  const { t } = await useTranslation(language, "translation");
   const title = splitPathname(pathname);
 
   return (
