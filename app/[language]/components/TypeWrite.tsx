f6fba503a3a619fafc003fa25781022c20630d45
YYGod <2569630663@qq.com>
2024-05-25 13:58:45 +0800

    Revert ":sparkles: (update react@19&&next14) 测试一下react compiler"
    This reverts commit 6fe1f3da3783d9da644042ae5dfe77fe3b8ddc5d.

--- app/[language]/components/TypeWrite.tsx
+++ app/[language]/components/TypeWrite.tsx
@@ -1,21 +1,14 @@
-"use client";
-// import { usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { ReactTyped } from "react-typed";
-import { Suspense, use } from "react";
-import { DailyWord } from "@/utils/getDailyWord";
-export function MyTypeWrite({
-  language,
-  wordsFetch,
-}: {
-  language: string;
-  wordsFetch: Promise<DailyWord>;
-}) {
-  const words = use(wordsFetch);
-  console.log(wordsFetch, words);
-  const word = language === "zh-CN" ? words.note : words.content;
+import { splitPathname } from "@/utils/dealPathname";
+import { useTranslation } from "@/app/i18n/client";
+export function MyTypeWrite({ language }: { language: string }) {
+  var pathname = usePathname();
+  var { t } = useTranslation(language, "translation");
+  var title = splitPathname(pathname);
   return (
     <ReactTyped
-      strings={[word]}
+      strings={[t(title)]}
       typeSpeed={50}
       style={{
         display: "flex",
