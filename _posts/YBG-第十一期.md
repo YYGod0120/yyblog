42d28cf7af1680e4875a2a231b4e81240419f84d
YYGod <2569630663@qq.com>
2024-06-03 19:38:01 +0800

    docs: new essay

---
+++ _posts/YBG-第十一期.md
@@ -0,0 +1,54 @@
+---
+title: YBG-第十一期
+date: 2024-06-03
+categories: Weekly
+excerpt: 这里是每周总结，记录每一周的趣事，技术分享以及学习生活。
+---
+
+> 一下又断更好久好久好久
+> 不知不觉中又到期末周
+
+一起来看看这段时间又有哪些技术
+
+## Technology/Study
+
+1. 随着React19发布，众多框架随之更新迭代。
+
+   [Next15RC](https://nextjs.org/blog/next-15-rc)：优化水合错误警告，增加React19Api，支持Compiler等等
+   [Astro4.9](https://nextjs.org/blog/next-15-rc)
+
+2. Tailwind真的很好用，这里推荐几个更好用的TailwindClass
+   [Tailwind Classes](https://mp.weixin.qq.com/s/RtSCz7YnwWxDo6sXWkKIzg)
+
+3. [一些值得一看的前端代码片段](https://mp.weixin.qq.com/s/778hr32KnLFEw7oBNt38sg)
+
+4. [React Conf大会上的五个重要事情](https://mp.weixin.qq.com/s/d9hAn5ggahyn_9IuqTUYNw):
+
+   - React Compiler 开源
+   - Remix 与 React Router 合并
+   - React 19 发布首个 RC
+   - Expo 即将支持通用服务端组件
+   - React 不会消失
+
+5. [HTML attributes vs DOM properties](https://jakearchibald.com/2024/attributes-vs-properties/)
+
+6. 如果有机会，有能力给React提pr说不定能用上[How to Contribute](https://legacy.reactjs.org/docs/how-to-contribute.html)
+
+## Reading/Watching
+
+**《平凡的世界》**，第一部读完，第二部已经读到一半了。
+
+比起第一部，第二部作者视角明显更专注于孙家，从第一部的一穷二白到第二部的稍有起色。证明了时代的洪流相比于个人的努力来的更重要。
+
+**选择比努力更重要。**
+
+**《美恐》**，胆子小不敢看原剧只能看b站精讲。但是越看越觉得好像没有那么恐怖。
+
+## Living
+
+外包似乎结束了，最后的钱也是没有给到我手上，估计也不会再给了。
+
+人生第一个实习面试就这么草草被KPI了，明明是内推却如此的敷衍了事。
+
+这段时间代码基本都在试用React19,挺容易上手的,效果也挺明显的就是存在一些bug(不知道是不是我的问题)
+我也出了一篇文章关于[React19-BETA食用手册](https://www.yygod0120.com/en-US/essay/2024-05-26/185)
---
+++ app/[language]/essay/2024-06-03/738/page.tsx
@@ -0,0 +1,73 @@
+
+  import Image from "next/image";
+import dynamic from "next/dynamic";
+  
+  // @ts-ignore
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// @ts-ignore
+import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import "@/app/[language]/essay/essay.css";
+export default function Page() {
+  const Comment = dynamic(() => import("@/app/[language]/components/Comment"), {
+    ssr: false,
+  });
+  return (
+    <div>
+    <div className="mt-8 bg-white flex flex-col items-start text-lg shadow-lg rounded-sm">
+    <span className="text-4xl text-left lg:px-20 md:px-[2.5vw] px-4 pt-12 text-visit-font font-bold">
+      YBG-第十一期
+    </span>
+    <span className="text-[#86909C] lg:px-20 pt-5 px-4 text-xl mb-5 md:px-[2.5vw]">
+      Categories: Weekly &nbsp;  2024-06-03
+    </span>
+    <div className="flex text-start break-words flex-col pb-12 lg:px-20 lg:w-[740px] md:w-[90vw] md:px-[2.5vw] w-[95vw] px-[2.5vw]">
+      <blockquote>
+<p>一下又断更好久好久好久
+不知不觉中又到期末周</p>
+</blockquote>
+<p>一起来看看这段时间又有哪些技术</p>
+<h2>Technology/Study</h2>
+<ol>
+<li><p>随着React19发布，众多框架随之更新迭代。</p>
+<p><a href="https://nextjs.org/blog/next-15-rc">Next15RC</a>：优化水合错误警告，增加React19Api，支持Compiler等等
+<a href="https://nextjs.org/blog/next-15-rc">Astro4.9</a></p>
+</li>
+<li><p>Tailwind真的很好用，这里推荐几个更好用的TailwindClass
+<a href="https://mp.weixin.qq.com/s/RtSCz7YnwWxDo6sXWkKIzg">Tailwind Classes</a></p>
+</li>
+<li><p><a href="https://mp.weixin.qq.com/s/778hr32KnLFEw7oBNt38sg">一些值得一看的前端代码片段</a></p>
+</li>
+<li><p><a href="https://mp.weixin.qq.com/s/d9hAn5ggahyn_9IuqTUYNw">React Conf大会上的五个重要事情</a>:</p>
+<ul>
+<li>React Compiler 开源</li>
+<li>Remix 与 React Router 合并</li>
+<li>React 19 发布首个 RC</li>
+<li>Expo 即将支持通用服务端组件</li>
+<li>React 不会消失</li>
+</ul>
+</li>
+<li><p><a href="https://jakearchibald.com/2024/attributes-vs-properties/">HTML attributes vs DOM properties</a></p>
+</li>
+<li><p>如果有机会，有能力给React提pr说不定能用上<a href="https://legacy.reactjs.org/docs/how-to-contribute.html">How to Contribute</a></p>
+</li>
+</ol>
+<h2>Reading/Watching</h2>
+<p><strong>《平凡的世界》</strong>，第一部读完，第二部已经读到一半了。</p>
+<p>比起第一部，第二部作者视角明显更专注于孙家，从第一部的一穷二白到第二部的稍有起色。证明了时代的洪流相比于个人的努力来的更重要。</p>
+<p><strong>选择比努力更重要。</strong></p>
+<p><strong>《美恐》</strong>，胆子小不敢看原剧只能看b站精讲。但是越看越觉得好像没有那么恐怖。</p>
+<h2>Living</h2>
+<p>外包似乎结束了，最后的钱也是没有给到我手上，估计也不会再给了。</p>
+<p>人生第一个实习面试就这么草草被KPI了，明明是内推却如此的敷衍了事。</p>
+<p>这段时间代码基本都在试用React19,挺容易上手的,效果也挺明显的就是存在一些bug(不知道是不是我的问题)
+我也出了一篇文章关于<a href="https://www.yygod0120.com/en-US/essay/2024-05-26/185">React19-BETA食用手册</a></p>
+
+    </div>
+    
+  </div>
+  <div className="mt-12 py-8 bg-white px-6">
+  <Comment />
+</div>
+    </div>
+  );
+}
