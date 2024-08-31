a8c3b0a6a51154ff02a0f85dfdac4a840b468910
YYGod <2569630663@qq.com>
2024-08-31 17:11:28 +0800

    FIX: FIX
    强制忽略冲突
    
    fix

--- utils/getDailyWord.ts
+++ utils/getDailyWord.ts
@@ -1,7 +1,30 @@
-import axios from "axios";
+export type DailyWord = {
+  sid: string;
+  note: string;
+  content: string;
+};
 
-export type DailyWord = Record<"sid" | "note" | "content", string>;
 export async function getDailyWord(): Promise<DailyWord> {
-  const res = await axios.get("http://open.iciba.com/dsapi/");
-  return res.data;
+  const url = "http://open.iciba.com/dsapi/";
+
+  try {
+    const response = await fetch(url);
+
+    // 检查 HTTP 响应状态
+    if (!response.ok) {
+      // 处理 HTTP 错误
+      const errorDetails = await response.text();
+      throw new Error(
+        `HTTP error! Status: ${response.status}, Details: ${errorDetails}`,
+      );
+    }
+
+    // 解析 JSON 响应
+    const data: DailyWord = await response.json();
+    return data;
+  } catch (error) {
+    // 捕获和处理任何其他错误
+    console.error("Failed to fetch daily word:", error);
+    throw new Error("Failed to fetch daily word");
+  }
 }
