a3d2af460e9c6dd3fc9e5a5be25e9deff7fd0336
YYGod <2569630663@qq.com>
2024-12-01 19:40:30 +0800

    docs: new essay

---
+++ app/[language]/essay/2024-11-17/994/page.tsx
@@ -0,0 +1,46 @@
+
+  import Image from "next/image";
+import dynamic from "next/dynamic";
+  
+  // @ts-ignore
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// @ts-ignore
+import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import "@/app/[language]/essay/essay.css";
+import { useTranslation } from "@/app/i18n";
+
+export default async function Page({
+  params: { language },
+}: {
+  params: { language: string };
+}) {
+  const { t } = await useTranslation(language, "essay-YBG-第二十四期");
+  return (
+    <div>
+    <div className="mt-8 bg-white flex flex-col items-start text-lg shadow-lg rounded-sm">
+    <span className="text-4xl text-left lg:px-20 md:px-[2.5vw] px-4 pt-12 text-visit-font font-bold">
+      YBG-第二十四期
+    </span>
+    <span className="text-[#86909C] lg:px-20 pt-5 px-4 text-xl mb-5 md:px-[2.5vw]">
+      Categories: Weekly &nbsp;  2024-11-17
+    </span>
+    <div className="flex text-start break-words flex-col pb-12 lg:px-20 lg:w-[740px] md:w-[90vw] md:px-[2.5vw] w-[95vw] px-[2.5vw]">
+      <blockquote>
+<p>{t("0")}</p>
+</blockquote>
+<h1>{t("1")}</h1>
+<p>{t("2")}<del>{t("3")}</del>{t("4")}</p>
+<p>{t("5")}</p>
+<p>{t("6")}</p>
+<p>{t("7")}</p>
+<h1>{t("8")}</h1>
+<p>{t("9")}<del>{t("10")}</del></p>
+<p>{t("11")}</p>
+<h1>{t("12")}</h1>
+<p>{t("13")}</p>
+    </div>
+    
+  </div>
+    </div>
+  );
+}
